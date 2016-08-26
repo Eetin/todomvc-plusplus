e34a21c37bdf83a8764909993b396c9da374b088
Dmitry Lebedev <p17dmitry@gmail.com>
2016-08-26 14:58:14 +0300

    modify test database

--- config/test.js
+++ config/test.js
@@ -4,6 +4,7 @@
 let config = {};
 
 // Use special purpose test database
-config.databaseUrl = 'postgres://localhost:5432/todos-test';
+// config.databaseUrl = 'postgres://localhost:5432/todos-test';
+config.databaseUrl = 'postgres://xwgpntqh:FlT7-jiI47ICjrl1hE7tPor1Nkln2faJ@horton.elephantsql.com:5432/xwgpntqh';
 
 module.exports = config;
